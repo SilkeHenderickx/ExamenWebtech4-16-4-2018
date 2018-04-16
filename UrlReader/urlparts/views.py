6468cd5ba4516ccd542b2eacf2c9fc47e095b287
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-04-16 15:30:16 +0200

    django done

--- UrlReader/urlparts/views.py
+++ UrlReader/urlparts/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from urlparse import urlparse
+from .models import Text
+from .forms import TextForm
 
 from django.shortcuts import render
 
@@ -12,9 +14,20 @@ def read_url(request, param):
     url = request.get_full_path()
     parse_object = urlparse(url)
     string = parse_object.path[9:]
-    newstring = string.replace("/", "***")
+    newstring = "";
+    if "//" not in string:
+        newstring = string.replace("/", "***")
+        data = {
+        'text': newstring,
+       }
 
-    return render(request, 'index.html', {'url': newstring })
+        form = TextForm(data)
+        if form.is_valid():
+            form.save(commit = True)
+
+
+    strings = Text.objects.all()
+    return render(request, 'index.html', {'strings': strings })
 
 def index(request):
     return render(request, 'index.html')
